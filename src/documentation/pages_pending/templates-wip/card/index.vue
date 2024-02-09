1a939a3d869a5151bea4205da49e21a0f19e63c3
ASanchLumen <Alejandro.Pineda@lumen.com>
2024-02-09 17:41:33 +0100

    [DPEDE-1327] Migrate chi documentation from nuxt2 to nuxt3

---
+++ src/documentation/pages_pending/templates-wip/card/index.vue
@@ -0,0 +1,25 @@
+<template lang="pug">
+  div
+    <TitleBar title="Card" description="A collection of templates built with responsive Cards." />
+    <Examples />
+</template>
+
+<script lang="ts">
+import { Vue } from 'vue-facing-decorator';
+import Examples from './examples/index.vue';
+
+@NuxtComponent({
+  head() {
+    return {
+      title: 'Chi - Card',
+    };
+  },
+  components: {
+    Examples,
+  },
+  layout() {
+    return 'wide';
+  },
+})
+export default class Card extends Vue {}
+</script>
